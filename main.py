4a882334cc7b626d4a6783258c73e6a631946e39
K Asante <73370911+kasante1@users.noreply.github.com>
2024-07-10 20:29:03 +0000

    ft: create post

--- main.py
+++ main.py
@@ -1,14 +1,21 @@
 from fastapi import FastAPI
 from fastapi.params import Body
+from pydantic import BaseModel
+from typing import Optional
 
 
 app = FastAPI()
 
+class PostValidator(BaseModel):
+    title: str
+    content: str
+    published: bool = True
+    rating: Optional[int] = None
 
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
 
 @app.post("/posts")
-async def create_posts(payLoad: dict = Body(...)):
-    return {"message": "post created successfully"}
+async def create_posts(payLoad: PostValidator):
+    return {"data": payLoad}
